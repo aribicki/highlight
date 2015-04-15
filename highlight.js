82a89040cdc7516899e0ef5e86c27cfe25785d20
Andrew Ribicki <aribicki@saintjoe.edu>
2015-04-15 11:16:25 -0500

    added new attr

--- highlight.js
+++ highlight.js
@@ -3,6 +3,7 @@ function highlightAllCode() { // creates function highlightAllCode()
   for (var i = 0; i < pres.length; i++) { // keeps track of lenth of pres
     var pre = pres[i]; // sets index
     var lang = pre.getAttribute("data-language"); // gets the language that is set with 
+    var lang = pre.getAttribute("data-language");
     if (languages.hasOwnProperty(lang)) // if language is set to other use that 
       highlightCode(pre, languages[lang]); 
   }
