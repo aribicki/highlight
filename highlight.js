4c521a6af98967222b596c0c43699c02f90b918e
Andrew Ribicki <aribicki@saintjoe.edu>
2015-04-15 11:17:18 -0500

    Update highlight.js

--- highlight.js
+++ highlight.js
@@ -1,3 +1,6 @@
+var languages = { // creates var languages
+  javascript: /\b(function|return|var|new|public)\b/g /* ... etc */ //looking for the following
+};
 function highlightAllCode() { // creates function highlightAllCode()
   var pres = document.body.getElementsByTagName("pre"); //sets var pres = document.body etc
   for (var i = 0; i < pres.length; i++) { // keeps track of lenth of pres
